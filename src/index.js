fed64ab58ddd3bac67fd637ef642ac9b06650c33
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-01-28 22:21:39 -0600

    auth works and directs to dashboard component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { Goat } from './components/Goat';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Goat />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
