0622f151265a594987cd9158d8c0b2f5f3a0fe1f
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-01-31 11:55:34 -0600

    all form imput captured in state

--- src/components/properties/PropertyForm.js
+++ src/components/properties/PropertyForm.js
@@ -1,43 +1,86 @@
 import React, { useState } from "react";
 
 export const PropertyForm = () => {
+   const [createdProperty, setCreatedProperty] = useState(
+        {
+           userId: localStorage.getItem("goat_user")
+        }
+    )
+    
    
-   
-   return (<>
-    <h2>Create a New Property</h2>
+   return (
+   <>
+   <h2>Create a New Property</h2>
     <form className="form--login">
         <fieldset>
             <label htmlFor="address">Address </label>
-            <input type="text" id="address" className="form-control" placeholder="4408 Goat Parkway"/>
+            <input onChange={(evt)=>{
+                const copy = {...createdProperty}
+                copy.address = evt.target.value
+                setCreatedProperty(copy)
+                }
+            }
+            type="text" id="address" className="form-control" placeholder="4408 Goat Parkway"/>
         </fieldset>
         <fieldset>
             <label htmlFor="bedroom">Number of Bedrooms </label>
-            <input type="number" id="bedroom" className="form-control" placeholder="1"/>
+            <input onChange={(evt)=>{
+                const copy = {...createdProperty}
+                copy.bedroom = evt.target.value
+                setCreatedProperty(copy)
+                }
+            }
+            type="number" id="bedroom" className="form-control" placeholder="1, 2, 3 etc."/>
         </fieldset>
         <fieldset>
             <label htmlFor="bath">Number of Bath</label>
-            <input type="number" id="bath" className="form-control" placeholder="1"/>
+            <input onChange={(evt)=>{
+                const copy = {...createdProperty}
+                copy.bath = evt.target.value
+                setCreatedProperty(copy)
+                }
+            }
+            type="number" id="bath" className="form-control" placeholder="1, 2, 3 etc."/>
         </fieldset>
         <fieldset>
             <label htmlFor="pets">Are Pets Allowed</label>
-            <select name="pets" id="pets" className="form-control">
-                <option value="volvo">Select "Yes" or "No"</option>
+            <select onChange={(evt)=>{
+                const copy = {...createdProperty}
+                copy.petsAllowed = evt.target.value
+                setCreatedProperty(copy)
+                }
+            }
+            name="pets" id="pets" className="form-control">
+                <option>Select "Yes" or "No"</option>
                 <option value={true}>Yes</option>
                 <option value={false}>No</option>
             </select>
         </fieldset>
         <fieldset>
             <label htmlFor="owner">Owner Name</label>
-            <input type="text" id="address" className="form-control" placeholder="Philip Black"/>
+            <input onChange={(evt)=>{
+                const copy = {...createdProperty}
+                copy.ownerName = evt.target.value
+                setCreatedProperty(copy)
+                }
+            }
+            type="text" id="address" className="form-control" placeholder="Philip Black"/>
         </fieldset>
         <fieldset>
             <label htmlFor="img">Image URL</label>
-            <input type="text" id="img" className="form-control" placeholder="https://YourImage.com/"/>
+            <input onChange={(evt)=>{
+                const copy = {...createdProperty}
+                copy.imgUrl = evt.target.value
+                setCreatedProperty(copy)
+                }
+            }
+            type="text" id="img" className="form-control" placeholder="https://YourImage.com/"/>
         </fieldset>        
         <fieldset>
             <button type="submit"> Register </button>
         </fieldset>
     </form>
-    </>)
+    </>
+    )
 
 }
