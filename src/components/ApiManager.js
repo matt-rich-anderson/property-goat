1f24b61c7bee5b68efdc7477fc09784a11eeed6c
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-01-30 20:30:13 -0600

    displaying all user properties

--- src/components/ApiManager.js
+++ src/components/ApiManager.js
@@ -1,1 +1,4 @@
-export const getUserProperties
+export const getCurrentUserProps = () => {
+    fetch(`http://localhost:8088/users/`)
+        .then(res => res.json())
+}
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,32 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import "./Dashboard.css"
 
 export const Dashboard = () => {
 
+    const [userProperties, setUserProperties] = useState({})
+
+    useEffect(() => fetch(`http://localhost:8088/users/${localStorage.getItem("goat_user")}?_embed=props`).then(res => res.json())
+                .then((data) => setUserProperties(data)),
+        []
+    )
 
     return (
         <>
-        <h1>Dashboard</h1>
+            <div className="dash">
+                {userProperties.props?.map((property) =>
+                    <div key={property.id} className="prop__dash" >
+                        <div className="prop__img">    
+                            <img className="prop__img--details" src={property.imgUrl} alt="Property Image" width="300"/>
+                        </div> 
+                        <div className="prop__address">
+                            <h3 className="prop__address--details">{property.address}</h3>
+                        </div>
+                        <div className="prop__owner">
+                            <p className="prop__owner--details">Owned by {property.ownerName}</p>
+                        </div>
+                    </div>
+                )}
+            </div>
         </>
     )
 
