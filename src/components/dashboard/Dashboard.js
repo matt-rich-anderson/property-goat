912a7c83502cba058d7723f22ded3891ae2ab5c6
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-02-09 08:29:43 -0600

    dashbaord shows lease

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import { getUserProperty } from "../ApiManager";
+import { getProperties } from "../ApiManager";
 import "./Dashboard.css"
 
 export const Dashboard = () => {
 
-    const [userProperties, setUserProperties] = useState({})
+    const [userProperties, setUserProperties] = useState([])
     const [landingMessage, setLandingMessage] = useState("")
 
     useEffect(() => 
-        getUserProperty()
-        .then((data) => setUserProperties(data))
-        ,[]
+        getProperties()
+        .then((data) => {
+            const properties = data.filter((p) => p.userId === parseInt(localStorage.getItem("goat_user")))
+            setUserProperties(properties)
+        }        
+    )
+    ,[]
     )
 
     useEffect(() =>  {
-        if(userProperties.props < 1){setLandingMessage(`Please Click "Add Properties" to Create Your First Property Card`)}
+        if(userProperties < 1){setLandingMessage(`Please Click "Add Properties" to Create Your First Property Card`)}
         else{setLandingMessage(`Click on a Property Card to See Details`)}
-
     }
     ,[userProperties]
-)
+    )
 
 
     return (
@@ -29,7 +32,7 @@ export const Dashboard = () => {
                 <h2>{landingMessage}</h2>
             </div>
             <div className="dash">
-                {userProperties.props?.map((property) =>
+                {userProperties.map((property) =>
                     <article className="prop__dash" key={property.id}>
                         <div className="prop__img">    
                             <img className="prop__img--details" src={property.imgUrl} alt="Property Image" width="300"/>
@@ -39,6 +42,9 @@ export const Dashboard = () => {
                         </div>
                         <div className="prop__owner">
                             <p className="prop__owner--details">Owned by {property.ownerName}</p>
+                        </div>
+                        <div className="prop__lease">
+                            <p className="prop__lease--details">{property.leases.length < 1 ? "AVAILABLE" : "Leased"}</p>
                         </div>
                     </article>
                 )}
