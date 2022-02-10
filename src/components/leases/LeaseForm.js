dfefadd6ca2436534f2bdb7a98a21b608a328e88
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-02-10 14:50:33 -0600

    add lease form functional

--- src/components/leases/LeaseForm.js
+++ src/components/leases/LeaseForm.js
@@ -1,28 +1,54 @@
 import React, { useEffect, useState } from "react";
-import { getUserProperty } from "../ApiManager";
+import { useHistory } from "react-router-dom";
+import { getUserProperty, sendLease } from "../ApiManager";
 
 export const LeaseForm = () => {
 
     const [userProperties, setUserProperties] = useState({})
+    const [newLease, setNewLease] = useState({})
+
+    const history = useHistory()
 
     useEffect(() => 
     getUserProperty ().then((data) => setUserProperties(data))
     ,[]
 )
 
+    const sendNewLease = (evt) => {
+        evt.preventDefault()
+        sendLease(newLease).then(() => {
+            history.push(`/property/${newLease.propId}`)
+        })
+    }
+
     return(
         <>
             <h2>Create a New Lease</h2>
             <fieldset className="form--lab-field">
-                <select>
+                <select className="form-control"onChange={(evt)=>{
+                        const copy = {...newLease}
+                        copy.propId = parseInt(evt.target.value)
+                        setNewLease(copy)
+                        }
+                    }>
                     <option>Select a Property</option>
                     {userProperties.props?.map( (p) => <option key={p.id} value={p.id}>{p.address}</option>)}
                 </select>
                 <label>Renter's Name:</label>
-                <input type="text" className="form-control" placeholder="Philip Black"/>
+                <input type="text" className="form-control" placeholder="Philip Black" onChange={(evt)=>{
+                        const copy = {...newLease}
+                        copy.renterName = evt.target.value
+                        setNewLease(copy)
+                        }
+                    }/>
                 <label>Monthly Rent:</label>
-                <input type="number" className="form-control" placeholder="3500"/>
-                <button>Create Lease</button>
+                <input type="number" className="form-control" placeholder="3500" onChange={(evt)=>{
+                        const copy = {...newLease}
+                        copy.rent = parseInt(evt.target.value)
+                        setNewLease(copy)
+                        }
+                    }/>
+                <button onClick={sendNewLease} type="submit">Create Lease</button>
             </fieldset>
         </>
     )
